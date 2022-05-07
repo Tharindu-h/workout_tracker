3e4fc36efed9ea685d72630fc1ae8490f5c8c24c
Tharindu H <tharindudeshan500@gmail.com>
2022-05-07 18:19:05 -0400

    Create app workout and create db models for workouts

---
+++ workout/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Exercise)
+admin.site.register(Weight)
+admin.site.register(Workout)
---
+++ workout/models.py
@@ -0,0 +1,26 @@
+from pyexpat import model
+from django.db import models
+
+class Exercise(models.Model):
+    name        = models.CharField(max_length=70, verbose_name=('Name'))
+    description = models.CharField(max_length=256, blank=True, null=True, verbose_name=('Description'))
+    sets        = models.IntegerField(default=0, verbose_name=('Number of Sets'))
+    reps        = models.IntegerField(default=0, verbose_name=('Number of Reps'))
+    weight      = models.ForeignKey('Weight', verbose_name=('Weight'), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Weight(models.Model):
+    weight = models.IntegerField(default=0, verbose_name=('Weight'))
+
+    def __str__(self):
+        return self.weight
+
+class Workout(models.Model):
+    name        = models.CharField(max_length=70, verbose_name=('Name'))
+    description = models.CharField(max_length=256, blank=True, null=True, verbose_name=('Description'))
+    exercises   = models.ForeignKey('Exercise', verbose_name=('Exercises'), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
