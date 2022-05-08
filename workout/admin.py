a1fe928f1a35c2df1b24de47e3501eedb7c8779b
Tharindu H <tharindudeshan500@gmail.com>
2022-05-08 19:38:10 -0400

    More database changes

--- workout/admin.py
+++ workout/admin.py
@@ -4,3 +4,4 @@ from .models import *
 admin.site.register(Exercise)
 admin.site.register(Weight)
 admin.site.register(Workout)
+admin.site.register(Set)
--- workout/models.py
+++ workout/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Exercise(models.Model):
   name        = models.CharField(max_length=70, verbose_name=('Name'))
   description = models.CharField(max_length=256, blank=True, null=True, verbose_name=('Description'))
-  sets        = models.ForeignKey('Set', verbose_name=('Set'), on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
@@ -18,14 +17,19 @@ class Weight(models.Model):
 class Workout(models.Model):
   name        = models.CharField(max_length=70, verbose_name=('Name'))
   description = models.CharField(max_length=256, blank=True, null=True, verbose_name=('Description'))
-  exercises   = models.ForeignKey('Exercise', verbose_name=('Exercises'), on_delete=models.CASCADE)
   dateTime    = models.DateTimeField()
+  exercises   = models.ManyToManyField('Exercise', verbose_name=('Exercises'))
 
-class Meta:
-  ordering = ['-dateTime']
+  class Meta:
+    ordering = ['-dateTime']
+  
   def __str__(self):
     return self.name
 
 class Set(models.Model):
-  weight      = models.ForeignKey('Weight', verbose_name=('Weight'), on_delete=models.CASCADE)
+  weight      = models.ManyToManyField("Weight", verbose_name=("weight"))
+  exercise    = models.ForeignKey('Exercise', verbose_name=('exercise'), on_delete=models.CASCADE, null=True)
   reps        = models.IntegerField(default=0, verbose_name=('Number of Reps'))
+
+  def __str__(self):
+    return self.exercise.name +  "set of " + str(self.reps)
