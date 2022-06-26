eb4d4dc9f0ee5ab96a459c2cbac34b52dde88080
Tharindu H <tharindudeshan500@gmail.com>
2022-06-26 17:14:01 -0400

    Add sets to exercise dynamically using js

--- workout/static/workout/js/form.js
+++ workout/static/workout/js/form.js
@@ -1,15 +1,13 @@
 const addSet = document.querySelector("#addSet");
 addSet.addEventListener("click", cloneSetForm);
 function cloneSetForm(){
-  const clonedSet = document.getElementById("set1").cloneNode(true);
-  console.log(clonedSet);
-  const target = document.querySelector('#addSetButton');
-  target.before(clonedSet);
+  currSet = document.querySelectorAll('#exercise1 .set').length;
+  document.getElementById('addSetButton').insertAdjacentHTML("beforebegin", getSetForm(currSet + 1));
 }
 
 
 function removeClickHandler(e) {
-  if (e.target.matches('#removeSet')) {
+  if (e.target.matches('.removeSet')) {
     console.log(e.target.parentNode.parentNode);
     e.target.parentNode.parentNode.remove();
   }
@@ -19,3 +17,24 @@ const exercise1 = document.querySelector('#exercise1');
 
 // add a single listener on list item
 exercise1.addEventListener('click', removeClickHandler);
+
+
+function getSetForm(currSet){
+  let setForm = '<div class="row set" id="set' + currSet +'">'+
+                    '<div class="col-2">'+
+                      '<label>Set ' + currSet + '</label>'+
+                    '</div>'+
+                    '<div class="col-3">'+
+                      '<input type="text" class="form-control" name="E1S1-weight"></input>'+
+                    '</div>'+
+                    '<div class="col-3">'+
+                      '<input type="text" class="form-control" name="E1S1-reps"></input>'+
+                    '</div>'+
+                    '<div class="col-2">'+
+                      '<button type="button" class="btn btn-outline-danger removeSet">Remove Set'+
+                      '</button>'+
+                    '</div>'+
+                  '</div>';
+    return setForm;
+  }
+  
