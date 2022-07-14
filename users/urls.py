cf0602a807e35aad90796d71c1cbcb2e1a161a56
Tharindu-h <tharindudeshan500@gmail.com>
2022-07-14 12:05:14 -0400

    Add password reset view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from urllib.parse import urlparse
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from .views import profile
 
 urlpatterns = [
-  path('profile/', profile, name='profile')
+  path('profile/', profile, name='profile'),
+  path('password-reset/', 
+    auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), 
+    name='password_reset')
 ]
