eaba298ceefa9afec15078467ce3badcb3ce20b4
Tharindu-h <tharindudeshan500@gmail.com>
2022-07-15 15:15:57 -0400

    Ensure the queryset returned by the API view is valid

--- workout/api.py
+++ workout/api.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
+from django.db.models import Q
 from .serializers import ExerciseTypeSerializer
 from .models import ExerciseType
 
 class ExerciseTypeAPI(viewsets.ReadOnlyModelViewSet):
-  queryset         = ExerciseType.objects.all()
   serializer_class = ExerciseTypeSerializer
   
+  def get_queryset(self):
+    return ExerciseType.objects.filter(Q(user__id=1) | Q(user__id=self.request.user.pk))
