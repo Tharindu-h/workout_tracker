78f14f63e1b74f3937cde126072e303ea1b9e62d
Tharindu-h <tharindudeshan500@gmail.com>
2022-08-04 13:21:34 -0400

    Remove unnecessary column 'description' from ExerciseType table

--- workout/models.py
+++ workout/models.py
@@ -52,7 +52,6 @@ def pre_delete_receiver(sender, instance, **kwargs):
 
 class ExerciseType(models.Model):
 	name        = models.CharField(verbose_name=("Exercise Name"), max_length=50)
-	description = models.CharField(verbose_name=("Description"), max_length=500, blank=True)
 	user        = models.ForeignKey(User, verbose_name=("User"), on_delete=models.CASCADE)
 
 	def __str__(self):
